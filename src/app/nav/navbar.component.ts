7d5d1e7e134f247c8b1db5660dab7978b0a34e3b
Neelamsetti <ramesh.neelamsetti@capgemini.com>
2018-08-24 12:58:10 +0530

    Test Commit

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'nav-bar',
-    templateUrl: `C:\New Project\ng-fundamentals\src\app\nav\navbar.component.html`,
+    templateUrl: `./navbar.component.html`,
     styles: [`
     .nav.navbar-nav {font-size: 15px;}
     #searchFrom {margin-right: 100px;}
