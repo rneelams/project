58e72be90971ab0260a43a9501b311f9b66ffb17
Neelamsetti <ramesh.neelamsetti@capgemini.com>
2018-08-24 11:37:17 +0530

    First commit

---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'nav-bar',
+    templateUrl: `C:\New Project\ng-fundamentals\src\app\nav\navbar.component.html`,
+    styles: [`
+    .nav.navbar-nav {font-size: 15px;}
+    #searchFrom {margin-right: 100px;}
+    @media (max-width: 1200px) {#searchFrom {display:none}}
+    `]
+})
+
+export class NavBarComponent {
+
+}
