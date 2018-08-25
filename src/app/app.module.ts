9f76769a0b89538310f676f4339a2ba5b6d14c84
Neelamsetti <ramesh.neelamsetti@capgemini.com>
2018-08-25 17:36:44 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-lists.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/Shared/event.service';
+
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [ EventService ],
    bootstrap: [EventsAppComponent]
 })
 
